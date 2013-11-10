88389a3dc2ae5c506c18c740f2891b10228db224
Zachary Owen <zowen@weblinc.com>
2013-11-10 17:06:46 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BaconAsia::Application.config.secret_key_base = '61cc26512b241d50328da05851d55663c06b7720e0d4ef736fe76dff8583732188f5f472395aca814c1a830215c4ab40fa56fbee0b995c435814dbf7abcae193'
